0eaceba07e67eb3b7119a0c147c62c7aaefe0642
Yaşar Arabacı <yasar11732@gmail.com>
2014-07-19 15:28:58 +0300

    Use lzma compression for cache files
    Signed-off-by: Yaşar Arabacı <yasar11732@gmail.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     description=lsystem.__doc__,
     long_description=open('README.rst').read(),
     url='https://github.com/yasar11732/tklsystem',
-    download_url='https://raw.githubusercontent.com/yasar11732/tklsystem/master/dist/lsystem-{}.zip'.format(lsystem.__version__),
+    download_url='https://github.com/yasar11732/tklsystem/tarball/{}'.format(lsystem.__version__),
     author='Yaşar Arabacı',
     author_email='yasar11732@gmail.com',
     entry_points = {
